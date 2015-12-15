48090bdfc2dd993665a3e083d20e180e693ceffa
Zou, Ying <xpzouying@gmail.com>
2015-12-16 00:00:34 +0800

    Use template in flask.

--- Python/Flask/zouyingblog/app/views.py
+++ Python/Flask/zouyingblog/app/views.py
@@ -1,14 +1,31 @@
 #/usr/bin/env python
+'''views.py
+
+Just for views.
+'''
 
 from app import first_app
+from flask import render_template
+
+
 @first_app.route('/')
 @first_app.route('/index')
 def index():
+    '''index
+
+    Show index page
+    '''
+
     return "Hello Zou, Ying - From blog"
 
 
 @first_app.route('/about')
 def about():
+    '''about
+
+    Show about page
+    '''
+
     return '''
     <html>
         <head>
@@ -20,3 +37,16 @@ def about():
         </body>
     </html>
     '''
+
+
+@first_app.route('/hello')
+def hello():
+    '''hello
+
+    Say hello to vister
+    '''
+    user = {'name': "Eden"}
+
+    return render_template("hello.html",
+                           title="Best_Title",
+                           user=user)
