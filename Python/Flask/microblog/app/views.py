b32a77b76e2f01ac2b2c1dc06cad7f1a3d31fdaa
Zou, Ying <xpzouying@gmail.com>
2015-12-17 00:31:05 +0800

    add flask template: for function.

--- Python/Flask/microblog/app/views.py
+++ Python/Flask/microblog/app/views.py
@@ -2,9 +2,19 @@
 
 
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello Flask!"
+
+
+@app.route('/hi_for')
+def hi_for():
+    posts = [{"name": "zouying", "info": "I'm zouying"},
+             {"name": "eden", "info": "Martin eden info."}]
+
+    return render_template("hi_for.html",
+                          posts=posts)
